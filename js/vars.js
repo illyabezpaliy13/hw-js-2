8d631cde46580a7d8bc58a96ccdd2b09a2832a6e
illyabezpaliy13 <110487623+illyabezpaliy13@users.noreply.github.com>
2023-03-09 21:55:44 +0200

    practice

---
+++ js/vars.js
@@ -0,0 +1,28 @@
+// let number = 10;
+// number = 5 + number;
+// console.log(number);
+
+// let elementWhidth = "50px";
+// console.log(elementWhidth);
+// Number.parseInt(elementWhidth);
+// console.log(Number.parseInt(elementWhidth));
+// elementWhidth = Number.parseInt(elementWhidth);
+// console.log(elementWhidth);
+
+// Number("5");
+// console.log(Number("5"));
+
+// let elementHeight = "200.55px";
+// console.log(elementHeight);
+// elementHeight = Number.parseFloat(elementHeight);
+// console.log( "це число у сотих", elementHeight);
+
+// let salary = "1000.12346";
+// salary = Number(salary);
+// salary = salary.toFixed(2);
+// console.log(salary);
+
+let quantity = "30";
+let message = "цей рядок неможливо привести до числа";
+console.log(Number(quantity));
+console.log(Number(message));
